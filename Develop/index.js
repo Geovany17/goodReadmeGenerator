6cdf0937c551a3dd8e9bd9b2be4b6dd2bebd1a9f
Geovany Cardoza <geoyd17@gmail.com>
2020-09-24 19:34:40 -0400

    almost done

--- Develop/index.js
+++ Develop/index.js
@@ -1,46 +1,124 @@
+// External NPM
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const util = require("util");
 
+// Internal modules
+const api = require("./utils/api.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // array of questions for user
 const questions = [
   {
     type: "input",
-    message: "What is your user name?",
     name: "username",
+    message: "What is your github user name?",
   },
   {
     type: "input",
+    name: "tittle",
     message: "What is your Project Tittle?",
-    name: "Tittle",
   },
   {
     type: "input",
+    name: "badge",
+    message: "Please provide the badges links that you want",
+  },
+  {
+    type: "input",
+    name: "description",
     message: "description about your project",
-    name: "Description",
   },
 
   {
     type: "input",
+    name: "installation",
     message: "Please provide the installation instructions",
-    name: "Installation",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide the project usage",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message:
+      "If applicable, provide guidelines on how other developers can contribute to your project.",
   },
   {
     type: "list",
+    name: "license",
     message: "Choose your license:",
-    name: "License",
-    choices: ["MIT", new inquirer.Separator(), "B"],
+    choices: [
+      "GNU AGPLv3",
+      new inquirer.Separator(),
+      "GNU GPLv3",
+      new inquirer.Separator(),
+      "GNU LGPLv3",
+      new inquirer.Separator(),
+      "Mozilla Public License 2.0",
+      new inquirer.Separator(),
+      "Apache License 2.0",
+      new inquirer.Separator(),
+      "MIT License",
+      new inquirer.Separator(),
+      "Boost Software License 1.0",
+      new inquirer.Separator(),
+      "The Unlicense",
+      new inquirer.Separator(),
+    ],
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide the project tests",
+  },
+
+  {
+    type: "input",
+    name: "repo",
+    message: "What is your repo link?",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success! Your README.md file has been generated");
+  });
+}
+
+// const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
-function init() {
-  inquirer.prompt(questions).then(function (response) {
-    console.log(`#response.username` + " " + response.username);
-  });
+async function init() {
+  try {
+    // Prompt Inquirer questions
+    const userResponses = await inquirer.prompt(questions);
+    console.log("Your responses: ", userResponses);
+    console.log(
+      "Thank you for your responses! Fetching your GitHub data next..."
+    );
+
+    // Call GitHub api for user info
+    const userInfo = await api.getUser(userResponses);
+    console.log("Your GitHub user info: ", userInfo);
+
+    // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+    console.log("Generating your README next...");
+    const markdown = generateMarkdown(userResponses, userInfo);
+    console.log(markdown);
+
+    // Write markdown to file
+    await writeFileAsync("ExampleREADME.md", markdown);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,35 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userResponses) {
+  return `# ${userResponses.title}\n
+
+  ${userResponses.badge}\n
+## Description 
+${userResponses.description}\n
+## Table of contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+## Installation
+        ${userResponses.installation}\n
+## Usage
+${userResponses.usage}\n
+## Licence
+${userResponses.licence}\n
+## Contributors
+${userResponses.contributing}\n
+## Test
+${userResponses.test}\n
+## Repository
+- [Project Repo](${userResponses.repo})\n
+## GitHub
+- ${userResponses.name}
+- [GitHub Profile](${userResponses.profile})
+- <${userResponses.email}>
 
 `;
 }
